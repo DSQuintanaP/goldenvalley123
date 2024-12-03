26dbcbb8a84ee34f89d350547b6778ff541077bd
QuintanaP D. Santiago <quintana.santiago1018@gmail.com>
2024-12-02 21:33:40 -0500

    Add files via upload

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            productos.Producto producto = new productos.Producto();
+            bool option5 = true;
+
+            while (option5)
+            {
+                Console.WriteLine("1. Agregar Producto");
+                Console.WriteLine("2. Leer Producto");
+                Console.WriteLine("3. Actualizar Producto");
+                Console.WriteLine("4. Eliminar Producto");
+                Console.WriteLine("5. Salir del Menu");
+
+                int option = int.Parse(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        producto.CrearProduct();
+                        break;
+                    case 2:
+                        producto.LeerProduct();
+                        break;
+                    case 3:
+                        producto.ActualizarProducto();
+                        break;
+                    case 4:
+                        producto.EliminarProduct();
+                        break;
+                    case 5:
+                        option5 = false;
+                        break;
+                    default:
+                        Console.WriteLine("Ingreso un numero incorrecto");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApp5/productos.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    public class productos
+    {
+        public int? _productId { get; set; }
+        public string _nombreProduct { get; set; }
+        public decimal _precioProduct { get; set; }
+        public int _cantidadProduct { get; set; }
+
+        public productos(int productId, string nombreProduct, decimal precioProduct, int cantidadProduct)
+        {
+            _productId = productId;
+            _nombreProduct = nombreProduct;
+            _precioProduct = precioProduct;
+            _cantidadProduct = cantidadProduct;
+        }
+
+        public class Producto
+        {
+            public List<productos> products = new List<productos>();
+
+            public int contadorId = 1;
+
+            /*------------------CREAR------------------------*/
+
+            public void CrearProduct()
+            {
+                Console.WriteLine("ingrese el nombre del producto");
+                string nombreProduct = Console.ReadLine();
+
+                Console.WriteLine("ingrese el precio del producto");
+                decimal precioProduct = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("ingrese la cantidad del producto");
+                int cantidadProduct = int.Parse(Console.ReadLine());
+
+                productos nuevoProduct = new productos(contadorId++, nombreProduct, precioProduct, cantidadProduct);
+                products.Add(nuevoProduct);
+            }
+
+            /*------------------LEER------------------------*/
+
+            public void LeerProduct()
+            {
+                foreach (var products in products)
+                {
+                    Console.WriteLine($"ID: {products._productId}, Nombre:{products._nombreProduct}, Precio:{products._precioProduct}, Cantidad: {products._cantidadProduct}");
+                }
+
+            }
+
+            /*------------------ACTUALIZAR------------------------*/
+
+            public void ActualizarProducto()
+            {
+                Console.WriteLine($"ingrese el ID del producto a actualizar");
+                int productId = int.Parse(Console.ReadLine());
+
+                productos producto = products.FirstOrDefault(p => p._productId == productId);
+
+                if (producto != null)
+                {
+                    Console.WriteLine("ingrese el nuevo nombre del producto");
+                    string nombreProduct = Console.ReadLine();
+
+                    Console.WriteLine("Ingrese el nuevo precio del producto");
+                    decimal precioProduct = decimal.Parse(Console.ReadLine());
+
+                    Console.WriteLine("ingrese la nueva Cantidad del producto");
+                    int cantidadProduct = int.Parse(Console.ReadLine());
+
+                    producto._nombreProduct = nombreProduct;
+                    producto._precioProduct = precioProduct;
+                    producto._cantidadProduct = cantidadProduct;
+
+                    Console.WriteLine("Producto actualizado correctamente");
+                }
+                else
+                {
+                    Console.WriteLine("producto no encontrado");
+                }
+            }
+
+            /*------------------BORRAR------------------------*/
+
+            public void EliminarProduct()
+            {
+                Console.WriteLine("ingrese el ID del producto que desea eliminar");
+                int productIdId = int.Parse(Console.ReadLine());
+                productos producto = products.FirstOrDefault(p => p._productId == productIdId);
+
+                if (producto != null)
+                {
+                    products.Remove(producto);
+                    Console.WriteLine("Producto borrado");
+                }
+                else
+                {
+                    Console.WriteLine("Producto no encontrado");
+                }
+            }
+        }
+    }
+}
